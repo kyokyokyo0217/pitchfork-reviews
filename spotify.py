633ec8d7b7bc5320cb2ef267e5fb03661efa9e89
Kyotaro Ishikawa <ishikawa.kyotaro@plus-zero.co.jp>
2022-03-14 14:54:50 +0900

    spotify classification

--- spotify.py
+++ spotify.py
@@ -4,51 +4,55 @@ from dotenv import dotenv_values
 import base64
 import json
 
-config = dotenv_values(".env")
-client_id = config["SPOTIFY_CLIENT_ID"]
-client_secret = config["SPOTIFY_CLIENT_SECRET"]
-spotify_access_token_url = "https://accounts.spotify.com/api/token"
+class Spotify():
+    spotify_access_token_url = "https://accounts.spotify.com/api/token"
+    ACCESS_TOKEN = ""
 
-def main(album_title, artist):
-    tmp = client_id + ":" + client_secret
-    # TODO 冗長かも？binaryの勉強
-    b_tmp = tmp.encode()
-    b64encoded = base64.b64encode(b_tmp).decode()
+    def authorize(self):
+        config = dotenv_values(".env")
+        client_id = config["SPOTIFY_CLIENT_ID"]
+        client_secret = config["SPOTIFY_CLIENT_SECRET"]
+        tmp = client_id + ":" + client_secret
+        # TODO 冗長かも？binaryの勉強
+        b_tmp = tmp.encode()
+        b64encoded = base64.b64encode(b_tmp).decode()
 
-    headers = {
-        "Authorization": "Basic " + b64encoded,
-        "Content-type": "application/x-www-form-urlencoded"
-    }
+        headers = {
+            "Authorization": "Basic " + b64encoded,
+            "Content-type": "application/x-www-form-urlencoded"
+        }
 
-    data = {
-        "grant_type": "client_credentials"
-    }
+        data = {
+            "grant_type": "client_credentials"
+        }
 
-    res = requests.post(
-        spotify_access_token_url,
-        headers=headers,
-        data=data
-    ).json()
+        res = requests.post(
+            self.spotify_access_token_url,
+            headers=headers,
+            data=data
+        ).json()
 
-    access_token = res["access_token"]
+        self.ACCESS_TOKEN = res["access_token"]
 
-    headers = {
-        "Authorization": "Bearer " + access_token,
-        "Content-Type": "application/json"
-    }
+    def search_album(self, album_title: str, artist: str):
+        headers = {
+            "Authorization": "Bearer " + self.ACCESS_TOKEN,
+            "Content-Type": "application/json"
+        }
 
-    # エスケープとか考慮してない
-    # TODO artistが二人以上の場合失敗する？
-    search_url = f"https://api.spotify.com/v1/search?q={album_title}+artist:{artist}&type=album"
+        # エスケープとか考慮してない
+        # TODO artistが二人以上の場合失敗する？
+        search_url = f"https://api.spotify.com/v1/search?q={album_title}+artist:{artist}&type=album"
 
-    result = requests.get(
-        url=search_url,
-        headers=headers
-    )
+        result = requests.get(
+            url=search_url,
+            headers=headers
+        )
 
-    # print(json.dumps(result.json(), indent=2))
-    link = result.json()["albums"]["items"][0]["external_urls"]["spotify"]
-    return link
+        return result
 
-if __name__ == "__main__":
-    main()
+    def get_album_link(self, album_title: str, artist: str):
+        query_result = self.search_album(album_title, artist)
+        # print(json.dumps(query_result.json(), indent=2))
+        link = query_result.json()["albums"]["items"][0]["external_urls"]["spotify"]
+        return link
