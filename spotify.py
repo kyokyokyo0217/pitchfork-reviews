f148b9764295f64b6b51439107e11ae61b9a7946
Kyotaro Ishikawa <ishikawa.kyotaro@plus-zero.co.jp>
2022-03-14 14:22:16 +0900

    billie eilisshでハードコーディング アルバム検索->url取得まで

--- spotify.py
+++ spotify.py
@@ -2,13 +2,14 @@ from email import header
 import requests
 from dotenv import dotenv_values
 import base64
+import json
 
 config = dotenv_values(".env")
 client_id = config["SPOTIFY_CLIENT_ID"]
 client_secret = config["SPOTIFY_CLIENT_SECRET"]
 spotify_access_token_url = "https://accounts.spotify.com/api/token"
 
-def main():
+def main(album_title="", artist=""):
     tmp = client_id + ":" + client_secret
     # TODO 冗長かも？binaryの勉強
     b_tmp = tmp.encode()
@@ -23,24 +24,38 @@ def main():
         "grant_type": "client_credentials"
     }
 
-    result = requests.post(
+    res = requests.post(
         spotify_access_token_url,
         headers=headers,
         data=data
     ).json()
 
-    access_token = result["access_token"]
+    access_token = res["access_token"]
 
     headers = {
-        "Authorization": "Bearer " + access_token
+        "Authorization": "Bearer " + access_token,
+        "Content-Type": "application/json"
     }
 
-    songs = requests.get(
-        'https://api.spotify.com/v1/tracks/2TpxZ7JUBn3uw46aR7qd6V',
+    album_title = "When We All Fall Asleep, Where Do We Go?"
+    artist = "Billie Eilish"
+
+    # エスケープとか考慮してない
+    # TODO artistが二人以上の場合失敗する？
+    search_url = f"https://api.spotify.com/v1/search?q={album_title}+artist:{artist}&type=album"
+
+    result = requests.get(
+        url=search_url,
         headers=headers
     )
 
-    print(songs.json())
+    print(json.dumps(result.json(), indent=2))
+
+    link = result.json()["albums"]["items"][0]["external_urls"]["spotify"]
+
+    print(link)
+
+    return link
 
 
 
